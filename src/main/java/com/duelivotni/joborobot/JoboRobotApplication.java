243c269c7fb880d94f55845a76820b59a88f2f68
Duelivotni <souji07031988@gmail.com>
2023-03-26 23:03:23 +0600

    auth via github
    Signed-off-by: Duelivotni <souji07031988@gmail.com>

---
+++ src/main/java/com/duelivotni/joborobot/JoboRobotApplication.java
@@ -0,0 +1,13 @@
+package com.duelivotni.joborobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoboRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoboRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duelivotni/joborobot/JoboRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duelivotni.joborobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoboRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
