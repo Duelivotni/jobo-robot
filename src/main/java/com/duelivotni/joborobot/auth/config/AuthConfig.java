84d7962b3410a3bfd1a2a77326120f9536131048
Duelivotni <souji07031988@gmail.com>
2023-03-31 00:33:38 +0600

    auth failure + google oauth
    Signed-off-by: Duelivotni <souji07031988@gmail.com>

--- src/main/java/com/duelivotni/joborobot/auth/config/AuthConfig.java
+++ src/main/java/com/duelivotni/joborobot/auth/config/AuthConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -12,6 +13,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
         http.authorizeRequests(
                 a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
@@ -21,6 +23,10 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
                 )
                 .logout(l -> l.logoutSuccessUrl("/").permitAll())
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .oauth2Login();
+                .oauth2Login(o -> o.failureHandler((request , response , exception) ->
+                {
+                    request.getSession().setAttribute("error message", exception.getMessage());
+                    handler.onAuthenticationFailure(request, response, exception);
+                }));
     }
 }
